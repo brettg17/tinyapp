416155989767399761bddf53c2233fe97cb4104d
brettg17 <brettgelinas7@gmail.com>
2024-02-29 09:51:57 -0600

    followed along compass to add ejs and html to urls_index.ejs file

--- express_server.js
+++ express_server.js
@@ -25,7 +25,10 @@ app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
 
-
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", tempalteVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port${PORT}!`);
