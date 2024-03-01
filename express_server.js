122fcd28a740f76576600d2901f5a95ca175a305
brettg17 <brettgelinas7@gmail.com>
2024-03-01 17:25:34 -0600

    created urls_show.ejs file and updated express_server.js with GET and POSt routes

--- express_server.js
+++ express_server.js
@@ -5,6 +5,8 @@ const PORT = 8080; //default port 8080
 //Usee EJS as templating engine
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -19,6 +21,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]}
    res.render("urls_show", templateVars);
@@ -30,6 +41,17 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+function generateRandomString() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port${PORT}!`);
 });
+
